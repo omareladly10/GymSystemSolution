008098d31d98890468a044f33f85e006f09d6d11
omareladly10 <omareladly434@gmail.com>
2026-06-06 18:25:23 +0300

    Gym S3 , V4

---
+++ GymSystem.BLL/ٍServices/Classes/MemberServices.cs
@@ -0,0 +1,15 @@
+﻿using GymSystem.BLL._ٍServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.BLL._ٍServices.Classes
+{
+    public class MemberServices : IMemberServices
+    {
+
+
+    }
+}
---
+++ GymSystem.BLL/ٍServices/Interfaces/IMemberServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.BLL._ٍServices.Interfaces
+{
+public interface IMemberServices
+    {
+
+
+
+    }
+}
