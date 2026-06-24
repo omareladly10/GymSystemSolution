e1adc3d4f37431c3837f97dcf0744c1e850ee330
omareladly10 <omareladly434@gmail.com>
2026-06-24 16:16:16 +0300

    Fixed Folder Services

---
+++ GymSystem.BLL/Common/Result.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.BLL.Common
+{
+    public record Result (bool Success, string? Error = null , ResultKind Kind = ResultKind.Ok)
+    {
+        public static Result ok() => new Result(true);
+        
+       
+        public static Result fail(string errorMessage , ResultKind kind = ResultKind.Conflict )=>new Result(false , errorMessage, kind); 
+    
+        
+
+        public static Result NotFound (string errorMessage = "Not Found") =>new Result(false , errorMessage , ResultKind.NotFound);
+
+
+        public static Result Validation(string erroorMessage) => new Result(false, erroorMessage, ResultKind.ValidationFailed);
+    
+    }
+
+    public record Result<T>(bool Success, T? Value , string ? Error = null ,ResultKind kind =ResultKind.Ok)
+    {
+        public static Result<T> Ok(T Value) => new(true, Value);
+
+        public static Result <T> Fail (string errorMessage , ResultKind kind =ResultKind.Conflict)=>new (false,default,errorMessage,kind);
+
+
+        public static Result <T> NotFound (string errorMessage = "Not Found")=>new (false,default,errorMessage,ResultKind.NotFound);
+
+
+    }
+
+    public enum ResultKind
+    { 
+    
+        Ok,
+        NotFound,
+        Conflict,
+        ValidationFailed,
+        Forbidden
+    
+    }
+
+
+}
